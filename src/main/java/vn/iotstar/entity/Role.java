bccd711481c7780bbe41e309dade2f694d9d6478
Tri Thai <NguyenTT162.4@gmail.com>
2026-09-22 08:31:51 +0700

    Buoc 4: Tao cac Entity Role va User

---
+++ src/main/java/vn/iotstar/entity/Role.java
@@ -0,0 +1,25 @@
+package vn.iotstar.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 30)
+    private String name;
+
+    public Role(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/vn/iotstar/entity/User.java
@@ -0,0 +1,41 @@
+package vn.iotstar.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 120)
+    private String email;
+
+    @Column(nullable = false, length = 150)
+    private String password;
+
+    @Column(nullable = false, length = 120, columnDefinition = "nvarchar(120)")
+    private String fullName;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private boolean enabled = true;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "role_id", nullable = false)
+    private Role role;
+}
