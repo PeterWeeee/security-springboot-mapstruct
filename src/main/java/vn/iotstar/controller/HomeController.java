104a0492ab2c4f468bd010bf2586418c056094ac
Tri Thai <NguyenTT162.4@gmail.com>
2026-09-22 10:11:07 +0700

    Buoc 11 (Vi du 3): Tao day du cac Controller Auth, User, Product, Home va Error

--- src/main/java/vn/iotstar/controller/HomeController.java
+++ src/main/java/vn/iotstar/controller/HomeController.java
@@ -1,18 +1,23 @@
 package vn.iotstar.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import vn.iotstar.service.ProductService;
+import vn.iotstar.service.UserService;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
-    @GetMapping({"/", "/dashboard"})
-    public String home() {
-        return "home";
-    }
+    private final UserService userService;
+    private final ProductService productService;
 
-    @GetMapping("/access-denied")
-    public String accessDenied() {
-        return "access-denied";
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("userCount", userService.countUsers());
+        model.addAttribute("productCount", productService.countProducts());
+        return "home";
     }
 }
